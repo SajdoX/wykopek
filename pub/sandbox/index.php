67af7d0f0a949045fddba11510f80183c40c6fdf
SajdoX <sidlo.kuba@gmail.com>
2023-04-23 11:13:22 +0200

    File type check

--- pub/sandbox/index.php
+++ pub/sandbox/index.php
@@ -21,17 +21,30 @@
     {
         //folder do którego trafiają zdjecia
         $targetDir = "img/";
+
         //pobiera nazwę pliku z $_FILES
         $sourceFileName = $_FILES['uploadedFile']['name'];
+
         //pobiera tymczasową ścieżkę do pliku na serwerze
         $tempUrl = $_FILES['uploadedFile']['tmp_name'];
+
+        //sprawdź czy przesłany plik to obraz
+        $imgInfo = getimagesize($tempUrl);
+        if(!is_array($imgInfo)) {
+            die("ERROR: File you try to upload is not a image!");
+        }
+
         //tworzy URL pliku na serwerze
         $targetUrl = $targetDir . $sourceFileName;
+
         if(file_exists($targetUrl)){
             die('ERROR: There is already file with the same name!');
         }
+
         //przesuwa plik do /img
         move_uploaded_file($tempUrl, $targetUrl);
+
+        echo "File uploaded!";
     }
 
     ?>
